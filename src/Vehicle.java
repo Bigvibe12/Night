7f14f7d610d93d3096c4ef3c8973fcb44fc1bd40
Bigvibe12 <bigvibessafaris@gmail.com>
2025-11-27 09:26:42 +0300

    Subclass 2

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,14 +1,19 @@
-abstract public class Vehicle {
-    protected String id;
-    protected String name;
+class Vehicle extends GeneralObject {
+    private String model;      // Unique attribute
+    private int year;          // Unique attribute
 
-    public Vehicle(String id, String name) {
-        this.id = id;
-        this.name = name;
+    public Vehicle(String name, String category, String model, int year) {
+        super(name, category);
+        this.model = model;
+        this.year = year;
     }
-    abstract public void displayDetails();
 
-    public double computeValue(double fuel) {
-        return fuel * fuel;
+    @Override
+    public void displayDetails() {
+        System.out.println("=== VEHICLE DETAILS ===");
+        System.out.println("Name: " + name);
+        System.out.println("Category: " + category);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
     }
 }
